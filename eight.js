05f6a2b58367a545d2708dc02f7e3135e577c2ed
atharvazope2715 <atharva.zope23@pccoepune.org>
2024-07-19 17:17:43 +0530

    Update eight.js

--- eight.js
+++ eight.js
@@ -5,7 +5,7 @@ function mergeArrays(arr1, arr2)
     return mergedArray;
 }
 
-const array1 = [1, 2, 3];
+const array1 = [9, 8, 7];
 const array2 = [4, 5, 6];
 const result = mergeArrays(array1, array2);
 console.log(result);
